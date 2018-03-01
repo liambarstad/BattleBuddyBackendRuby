26624c5ffcfd84745c2e2e321dd4372054f59b02
Liam Barstad <liambarstad@gmail.com>
2018-02-28 20:34:18 -0700

    switch to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180301014120) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alarms", force: :cascade do |t|
     t.boolean "active", default: false
     t.string "time"
